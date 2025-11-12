98838f62b7e22216642f1e0d5316b24696084cee
Wasim Akhtar Khan <122202006+wasim2451@users.noreply.github.com>
2025-11-12 08:59:04 +0000

    Added Logan Route

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>{
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
     res.send(`<h1>Hello World From Wasim Tech Park</h1>
         <p>This is me trying to create a PR on Company repo !</p>
         `);
 });
-app.get('/wasimSritama',(req,res)=>{
+app.get('/wasimSritama', (req, res) => {
     res.send("Sritama is Beautiful | Wasim is too !");
+});
+app.get('/logan', (req, res) => {
+    res.send("Hello World from Logan ");
 })
-app.listen(3000,(message)=>{
+
+app.listen(3000, (message) => {
     console.log("Listenting on 3000");
 });
