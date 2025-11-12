a659dcc0493c3638b94c2576c60c777263df5e47
Wasim Akhtar Khan <122202006+wasim2451@users.noreply.github.com>
2025-11-12 07:03:43 +0000

    Added Nodemon

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
-    res.send(`<h1>Hello World From Wasim Tech Park</h1>`);
+    res.send(`<h1>Hello World From Wasim Tech Park</h1>
+        <p>This is me trying to create a PR on Company repo !</p>
+        `);
 });
+app.get('/wasim',(req,res)=>{
+    res.send("Sritama is Beautiful | Wasim is too !");
+})
 app.listen(3000,(message)=>{
     console.log("Listenting on 3000");
 });
